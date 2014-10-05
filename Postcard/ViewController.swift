1be19a4a45a6b41d3176a2f2895d9f98057fa826
David Blanck <dblanck@wowway.com>
2014-10-05 16:12:03 -0400

    Simple Cleanup

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
                             
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    
     @IBOutlet weak var mailButton: UIButton!
-    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
